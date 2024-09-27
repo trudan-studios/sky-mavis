d1b7133dc95e3efee37995c37348acbc32818f7c
Gerard Altamirano <hello@itsgerard.com>
2024-09-27 20:51:02 +0200

    feat: Group events by api key

--- mavis-aggregator/src/index.ts
+++ mavis-aggregator/src/index.ts
@@ -4,10 +4,16 @@ import Redis from "ioredis";
 const MAX_BODY_SIZE = 1e6; // 1MB in bytes
 const MAX_REQUESTS_PER_SECOND = 100;
 const FLUSH_INTERVAL = 1000; // 1 second in milliseconds
+const MAVIS_API_URL = "https://x.skymavis.com/track";
 
 type Event = {
     type: string,
     data: Record<string, any>,
+}
+
+type QueuedEvent = {
+    api_key: string;
+    event: Event,
 }
 
 type TrackEventBody = {
@@ -20,7 +26,6 @@ let lastRequestTime = Date.now();
 const redis = new Redis(Number(process.env.REDIS_PORT ?? 6379), process.env.REDIS_HOST!);
 
 const app = new Hono();
-
 const calculatePayloadSize = (events: Event[]): number => {
     return new TextEncoder().encode(JSON.stringify(events)).length;
 }
@@ -38,55 +43,63 @@ const flushEvents = async (): Promise<void> => {
         return;
     }
 
-    let eventsToSend: Event[] = [];
+    let eventsToSend: QueuedEvent[] = [];
     let currentSize = 0;
 
     while (true) {
         const nextEvent = await redis.lpop('event_queue');
         if (!nextEvent) break;
-        const event = JSON.parse(nextEvent) as Event;
-        const newSize = calculatePayloadSize([...eventsToSend, event]);
+        const queuedEvent = JSON.parse(nextEvent) as QueuedEvent;
+        const newSize = calculatePayloadSize([...eventsToSend.map(qe => qe.event), queuedEvent.event]);
         if (newSize > MAX_BODY_SIZE) {
             // Re-add the event to the queue
             await redis.lpush('event_queue', nextEvent);
             break;
         }
-        eventsToSend.push(event);
+        eventsToSend.push(queuedEvent);
+
         currentSize = newSize;
     }
 
     if (eventsToSend.length > 0) {
-        try {
-            if (process.env.MAVIS_API_KEY === undefined) {
-                console.error("MAVIS_API_KEY is not set");
-                return;
+        // Group events by API key in memory
+        const groupedEvents: Record<string, Event[]> = {};
+        for (const queuedEvent of eventsToSend) {
+            if (!groupedEvents[queuedEvent.api_key]) {
+                groupedEvents[queuedEvent.api_key] = [];
             }
-            if (process.env.MAVIS_API_URL === undefined) {
-                console.error("MAVIS_API_URL is not set");
-                return;
-            }
-            requestsInLastSecond++;
-            console.log(`Flushing ${eventsToSend.length} events (${currentSize} bytes)`);
-            const response = await fetch(process.env.MAVIS_API_URL!, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Basic ${btoa(process.env.MAVIS_API_KEY + ':')}`,
-                },
-                body: JSON.stringify({api_key: process.env.MAVIS_API_KEY, events: eventsToSend}),
-            });
-            console.log(`Flush response: ${response.status}`);
-            console.log(await response.json());
-            if (!response.ok) {
-                console.error(`Failed to flush events: ${response.status} ${await response.text()}`);
-            }
-        } catch(e)
-        {
-            console.error("Error flushing events", e);
-            // Re-add the events to the queue
-            // @ts-ignore
-            await redis.lpush('event_queue', ...eventsToSend.map(JSON.stringify));
+            groupedEvents[queuedEvent.api_key].push(queuedEvent.event);
         }
+
+        for (const apiKey in groupedEvents) {
+            await sendEvents(apiKey, groupedEvents[apiKey], currentSize);
+        }
+    }
+}
+const sendEvents = async (apiKey: string, events: Event[], size: number): Promise<void> => {
+    try {
+        requestsInLastSecond++;
+        console.log(`Flushing ${events.length} events (${size} bytes) using API key ${apiKey.slice(0, 5)}...${apiKey.slice(-5)}`);
+        const response = await fetch(MAVIS_API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${btoa(apiKey + ':')}`,
+            },
+            body: JSON.stringify({api_key: apiKey, events}),
+        });
+        console.log(`Flush response: ${response.status}`);
+        if (!response.ok) {
+            console.error(`Failed to flush events: ${response.status} ${await response.text()}`);
+        } else {
+            console.log(await response.json());
+        }
+    } catch (e) {
+        console.error("Error flushing events", e);
+        await redis.lpush(
+            'event_queue',
+            ...events.map(event => JSON.stringify({api_key: apiKey, event}))
+        );
     }
 }
 
@@ -104,8 +117,10 @@ app.post('/track', async (c) => {
     }
 
     try {
-        // @ts-ignore
-        await redis.rpush('event_queue', ...body.events.map(JSON.stringify));
+        await redis.rpush(
+            'event_queue',
+            ...body.events.map(event => JSON.stringify({api_key: body.api_key, event}))
+        );
         return c.json({success: true});
     } catch (e) {
         console.error(e);
